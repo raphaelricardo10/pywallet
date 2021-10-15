304f0b870a8c887c8035e3f326dbd5d3162e43a9
Raphael <raphaelricardo10@gmail.com>
2021-10-15 17:56:14 -0300

    Added class instantiation test with CPF

--- tests/testCustomerValidation.py
+++ tests/testCustomerValidation.py
@@ -6,3 +6,7 @@ class TestCustomerValidationMethods(unittest.TestCase):
     def test_cpf_validation(self):
         with self.assertRaises(ValueError):
             Customer.validateCpf("12312312365")
+
+    def test_class_initialization(self):
+        with self.assertRaises(ValueError):
+            Customer("12312312365")
