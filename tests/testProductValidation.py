ebdd59e5b3f1194f329872615b4b5cf291c743e9
Raphael <raphaelricardo10@gmail.com>
2021-10-15 19:22:50 -0300

    Added a test for negative value in constructor

--- tests/testProductValidation.py
+++ tests/testProductValidation.py
@@ -6,3 +6,7 @@ class TestProductValidationMethods(unittest.TestCase):
     def test_negative_value(self):
         with self.assertRaises(ValueError):
             Product.validateValue(-10)
+            
+    def test_constructor_validation(self):
+        with self.assertRaises(ValueError):
+            Product("A", -10)
