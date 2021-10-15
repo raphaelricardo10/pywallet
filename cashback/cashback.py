be1187489edc64c21111ea08d0051c16310229cf
Raphael <raphaelricardo10@gmail.com>
2021-10-15 13:34:17 -0300

    Negative cashback test passed

--- cashback/cashback.py
+++ cashback/cashback.py
@@ -1,2 +1,5 @@
 def calculateCashback(value: float, percent: float) -> float:
+    if percent < 0:
+        raise ValueError("Percent value cannot be less than zero")
+
     return value*percent/float(100)
--- tests/testCashback.py
+++ tests/testCashback.py
@@ -6,3 +6,7 @@ class TestCashbackMethods(unittest.TestCase):
     def test_cashback_ok(self):
         result = calculateCashback(150, 5)
         self.assertEqual(result, 7.5)
+
+    def test_negative_percent(self):
+        with self.assertRaises(ValueError):
+            result = calculateCashback(150, -5)
