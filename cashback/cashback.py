d896f46242beba852e265f9c28e1656fa6efc125
Raphael <raphaelricardo10@gmail.com>
2021-10-15 17:35:15 -0300

    File moved to new directory

--- cashback/cashback.py
+++
@@ -1,8 +0,0 @@
-def calculateCashback(value: float, percent: float) -> float:
-    if value < 0:
-        raise ValueError("The value cannot be less than zero")
-
-    if percent < 0:
-        raise ValueError("Percent value cannot be less than zero")
-
-    return value*percent/float(100)
