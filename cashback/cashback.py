6f5db9b0b862127b8d3826bfb19385e69c00a8d8
Raphael <raphaelricardo10@gmail.com>
2021-10-15 13:22:09 -0300

    Cashback test passed

---
+++ cashback/cashback.py
@@ -0,0 +1,2 @@
+def calculateCashback(value: float, percent: float) -> float:
+    return value*percent/float(100)
--- tests/testCashback.py
+++ tests/testCashback.py
@@ -1,7 +1,8 @@
 import unittest
+from cashback.cashback import *
 
 class TestCashbackMethods(unittest.TestCase):
     
     def test_cashback_ok(self):
-        result = calculateCashback(100, 5)
-        self.assertEqual(result, 5)
+        result = calculateCashback(150, 5)
+        self.assertEqual(result, 7.5)
