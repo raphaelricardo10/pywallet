77e847d7900554ff32cd20abd565cec4c351f965
Raphael <raphaelricardo10@gmail.com>
2021-10-16 00:37:12 -0300

    Added test to order require the customer and date

--- tests/testOrderValidation.py
+++ tests/testOrderValidation.py
@@ -1,6 +1,8 @@
 import unittest
+from datetime import datetime
 from pywallet.product import Product
 from pywallet.order import Order
+from pywallet.customer import Customer
 
 class TestOrderValidationMethods(unittest.TestCase):
     
@@ -15,3 +17,8 @@ class TestOrderValidationMethods(unittest.TestCase):
         order = Order()
         with self.assertRaises(AttributeError):
             order.total = 150
+
+    def test_constructor_ok(self):
+        customer = Customer("Raphael", "16149113710")
+        date = datetime(2026, 1, 2)
+        order = Order(customer, date)
