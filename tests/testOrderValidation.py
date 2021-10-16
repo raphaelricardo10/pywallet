eda2bb40fb9c06e70b0c2c86b0811b1bb1bfda83
Raphael <raphaelricardo10@gmail.com>
2021-10-16 00:40:11 -0300

    Refactored tests to create order with arguments

--- tests/testOrderValidation.py
+++ tests/testOrderValidation.py
@@ -7,14 +7,18 @@ from pywallet.customer import Customer
 class TestOrderValidationMethods(unittest.TestCase):
     
     def test_total_order_ok(self):
-        order = Order()
+        customer = Customer("Raphael", "16149113710")
+        date = datetime(2026, 1, 2)
+        order = Order(customer, date)
         order.addItem(Product("A", 10), 2)
         order.addItem(Product("B", 30), 3)
         
         self.assertEqual(order.total, 110)
 
     def test_reassign_total(self):
-        order = Order()
+        customer = Customer("Raphael", "16149113710")
+        date = datetime(2026, 1, 2)
+        order = Order(customer, date)
         with self.assertRaises(AttributeError):
             order.total = 150
 
