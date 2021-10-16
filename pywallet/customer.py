d3ea453eeb0c56b6ba4f4e0098d117db307f0ad5
Raphael <raphaelricardo10@gmail.com>
2021-10-16 00:48:34 -0300

    Changed CPF to document

--- pywallet/customer.py
+++ pywallet/customer.py
@@ -1,48 +1,48 @@
 class Customer:
 
-    def __init__(self, name: str, cpf: str) -> None:
+    def __init__(self, name: str, document: str) -> None:
         self.name = name
-        self.cpf = cpf
+        self.document = document
 
     class Error(Exception):
         """Base class for errors"""
         pass
 
-    class InvalidCpfError(Error):
+    class InvalidDocumentError(Error):
         """Raised when the input CPF is invalid"""
         pass
 
     @property
-    def cpf(self):
-        return self._cpf
+    def document(self):
+        return self._document
 
-    @cpf.setter
-    def cpf(self, value):
+    @document.setter
+    def document(self, value):
         try:
-            Customer.validateCpf(value)
+            Customer.validateDocument(value)
         except:
             raise
 
-        self._cpf = value
+        self._document = value
 
-    def validateCpf(cpf: str) -> None:
+    def validateDocument(document: str) -> None:
         #Get all digits from CPF string
-        numbers = [int(char) for char in cpf if char.isdigit()]
+        numbers = [int(char) for char in document if char.isdigit()]
 
         #Check if there are 11 digits
         if len(numbers) != 11:
-            raise Customer.InvalidCpfError
+            raise Customer.InvalidDocumentError
 
         """
         Check if all the numbers are the same
         This is a valid CPF with this algorithm, but it is not used
         """
         if numbers == numbers[::-1]:
-            raise Customer.InvalidCpfError
+            raise Customer.InvalidDocumentError
 
-        #Validate both cpf digits
+        #Validate both document digits
         for i in range(9, 11):
             value = sum((numbers[num] * ((i+1) - num) for num in range(0, i)))
             digit = ((value * 10) % 11) % 10
             if digit != numbers[i]:
-                raise Customer.InvalidCpfError
+                raise Customer.InvalidDocumentError
--- tests/testCustomerValidation.py
+++ tests/testCustomerValidation.py
@@ -3,10 +3,10 @@ from pywallet.customer import Customer
 
 class TestCustomerValidationMethods(unittest.TestCase):
 
-    def test_cpf_validation(self):
-        with self.assertRaises(Customer.InvalidCpfError):
-            Customer.validateCpf("12312312365")
+    def test_document_validation(self):
+        with self.assertRaises(Customer.InvalidDocumentError):
+            Customer.validateDocument("12312312365")
 
     def test_constructor_validation(self):
-        with self.assertRaises(Customer.InvalidCpfError):
+        with self.assertRaises(Customer.InvalidDocumentError):
             Customer("Raphael Ricardo", "12312312365")
