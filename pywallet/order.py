b442a3252745ba7dca146dc753a319d4f2e47f62
Raphael <raphaelricardo10@gmail.com>
2021-10-16 01:12:59 -0300

    Date validation passed

--- pywallet/order.py
+++ pywallet/order.py
@@ -3,7 +3,7 @@ from pywallet.product import Product
 from pywallet.customer import Customer
 
 class Order:
-    def __init__(self, customer: Customer, sold_at: datetime) -> None:
+    def __init__(self, customer: Customer, sold_at: str) -> None:
         self.customer = customer
         self.sold_at = sold_at
         self.items = []
@@ -12,6 +12,18 @@ class Order:
     @property
     def total(self):
         return self._total
+
+    @property
+    def sold_at(self):
+        return self._sold_at
+
+    @sold_at.setter
+    def sold_at(self, value):
+        try:
+            self._sold_at = datetime.strptime(value, "%Y-%m-%d %H:%M:%S")
+        except ValueError:
+            raise
+            
 
     class Item:
         def __init__(self, product: Product, qty: int) -> None:
