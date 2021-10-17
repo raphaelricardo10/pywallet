ff8f29fe3c4e4721e38f64036c0e2f6e4a966642
Raphael <raphaelricardo10@gmail.com>
2021-10-16 21:12:38 -0300

    Added test to apply cashback in a order

--- tests/testOrderValidation.py
+++ tests/testOrderValidation.py
@@ -10,7 +10,6 @@ class TestOrderValidationMethods(unittest.TestCase):
         order = Order(customer, "2026-01-02 00:00:00")
         order.addItem(Product("A", 10), 2)
         order.addItem(Product("B", 30), 3)
-        
         self.assertEqual(order.total, 110)
 
     def test_reassign_total(self):
@@ -27,3 +26,13 @@ class TestOrderValidationMethods(unittest.TestCase):
         customer = Customer("Raphael", "16149113710")
         with self.assertRaises(ValueError):
             Order(customer, "1213123-01-02 00:00:00")
+
+    def test_cashback_change(self):
+        customer = Customer("Raphael", "16149113710")
+        order = Order(customer, "2026-01-02 00:00:00", 5)
+        order.addItem(Product("A", 10), 5)
+        self.assertEqual(order.cashback, 2.5)
+        print(order.cashback)
+        order.addItem(Product("A", 20), 3)
+        self.assertEqual(order.cashback, 5.5)
+        print(order.cashback)
