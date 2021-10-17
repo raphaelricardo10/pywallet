d9b19bcbf2b8d135e9e5c5e38725261810b3e8a8
Raphael <raphaelricardo10@gmail.com>
2021-10-17 12:04:04 -0300

    Change dates to past

--- packageTests/testOrderValidation.py
+++ packageTests/testOrderValidation.py
@@ -5,29 +5,30 @@ class TestOrderValidationMethods(unittest.TestCase):
     
     def test_total_order_ok(self):
         customer = Customer("Raphael", "16149113710")
-        order = Order(customer, "2026-01-02 00:00:00")
+        order = Order(customer, "2020-01-02 00:00:00")
         order.addItem(Product("A", 10), 2)
         order.addItem(Product("B", 30), 3)
         self.assertEqual(order.total, 110)
 
     def test_reassign_total(self):
         customer = Customer("Raphael", "16149113710")
-        order = Order(customer, "2026-01-02 00:00:00")
+        order = Order(customer, "2020-01-02 00:00:00")
         with self.assertRaises(AttributeError):
             order.total = 150
 
     def test_constructor_ok(self):
         customer = Customer("Raphael", "16149113710")
-        order = Order(customer, "2026-01-02 00:00:00")
+        order = Order(customer, "2020-01-02 00:00:00")
 
     def test_invalid_date(self):
         customer = Customer("Raphael", "16149113710")
         with self.assertRaises(ValueError):
             Order(customer, "1213123-01-02 00:00:00")
 
+
     def test_cashback_change(self):
         customer = Customer("Raphael", "16149113710")
-        order = Order(customer, "2026-01-02 00:00:00", 5)
+        order = Order(customer, "2020-01-02 00:00:00", 5)
         order.addItem(Product("A", 10), 5)
         self.assertEqual(order.cashback, 2.5)
         print(order.cashback)
