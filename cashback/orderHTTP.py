428a112b99a1496ef5acc7cca5c0ef51c062851e
Raphael <raphaelricardo10@gmail.com>
2021-10-17 11:55:53 -0300

    Added no products constraint and total validation

--- cashback/orderHTTP.py
+++ cashback/orderHTTP.py
@@ -5,6 +5,18 @@ from sales.customer import Customer
 
 class orderHTTP(Order):
     def __init__(self, response: str, cashbackPercent=None) -> None:
-        obj = load(response)
-        customer = Customer(obj['customer']['name'], obj['customer']['document'])
-        super().__init__(customer, obj['sold_at'], items=obj['products'], cashbackPercent=cashbackPercent)
+
+        try:
+            obj = load(response)
+            customer = Customer(obj['customer']['name'], obj['customer']['document'])
+
+            if not obj['products']:
+                raise ValueError("A least one product is required to complete the order")
+
+            super().__init__(customer, obj['sold_at'], items=obj['products'], cashbackPercent=cashbackPercent)
+
+            if float(obj['total']) != self.total:
+                raise ValueError("The order total is different of the sum of products")
+        
+        except:
+            raise
