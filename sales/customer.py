810625df70bad3e8d35997848d03990878d3c2e7
Raphael <raphaelricardo10@gmail.com>
2021-10-17 13:06:07 -0300

    Added a filter to store only document digits

--- sales/customer.py
+++ sales/customer.py
@@ -19,11 +19,9 @@ class Customer:
     @document.setter
     def document(self, value):
         try:
-            Customer.validateDocument(value)
+            self._document = Customer.validateDocument(value)
         except:
             raise
-
-        self._document = value
 
     @property
     def name(self):
@@ -36,24 +34,32 @@ class Customer:
 
         self._name = value
 
-    def validateDocument(document: str) -> None:
-        #Get all digits from CPF string
-        numbers = [int(char) for char in document if char.isdigit()]
+    def validateDocument(document: str) -> str:
+        numberLst = []
+        digits = ""
+
+        #Transform all the digits of string in a list for better handling
+        for char in document:
+            if char.isdigit():
+                numberLst.append(int(char))
+                digits += char
 
         #Check if there are 11 digits
-        if len(numbers) != 11:
+        if len(numberLst) != 11:
             raise Customer.InvalidDocumentError
 
         """
         Check if all the numbers are the same
         This is a valid CPF with this algorithm, but it is not used
         """
-        if numbers == numbers[::-1]:
+        if numberLst == numberLst[::-1]:
             raise Customer.InvalidDocumentError
 
         #Validate both document digits
         for i in range(9, 11):
-            value = sum((numbers[num] * ((i+1) - num) for num in range(0, i)))
+            value = sum((numberLst[num] * ((i+1) - num) for num in range(0, i)))
             digit = ((value * 10) % 11) % 10
-            if digit != numbers[i]:
+            if digit != numberLst[i]:
                 raise Customer.InvalidDocumentError
+
+        return digits
