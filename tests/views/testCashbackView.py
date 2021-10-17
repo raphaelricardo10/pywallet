8b892e5c02e63fc62866eb982d53c30c784607de
Raphael <raphaelricardo10@gmail.com>
2021-10-17 14:41:43 -0300

    Moved test inputs folder

--- tests/views/testCashbackView.py
+++ tests/views/testCashbackView.py
@@ -7,7 +7,7 @@ from cashback.orderHTTP import orderHTTP
 class TestOrderHTTP(unittest.TestCase):
 
     def test_create_order_ok(self):
-        with open('cashback/testInputs/rightInputs.yaml') as json:
+        with open('tests/inputs/salesRightInputs.yaml') as json:
             for obj in load(json):
                 order = orderHTTP(dumps(obj['input']))
                 output = obj['output']
