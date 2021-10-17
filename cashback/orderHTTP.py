7886f6fac24ccb3c7724bf877dbf911c67f01253
Raphael <raphaelricardo10@gmail.com>
2021-10-17 11:15:42 -0300

    changed JSON to yaml

--- cashback/orderHTTP.py
+++ cashback/orderHTTP.py
@@ -1,10 +1,10 @@
 from sales import Order
-from json import loads
+from yaml import load
 
 from sales.customer import Customer
 
 class orderHTTP(Order):
     def __init__(self, response: str, cashbackPercent=None) -> None:
-        obj = loads(response)
+        obj = load(response)
         customer = Customer(obj['customer']['name'], obj['customer']['document'])
         super().__init__(customer, obj['sold_at'], items=obj['products'], cashbackPercent=cashbackPercent)
