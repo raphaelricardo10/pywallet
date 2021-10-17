ce3b0cbf61f222f269eb295bdf009f687e28595c
Raphael <raphaelricardo10@gmail.com>
2021-10-17 11:16:21 -0300

    Allowed testing from yaml file

--- cashback/tests.py
+++ cashback/tests.py
@@ -1,12 +1,16 @@
 import unittest
+from yaml import load
+from json import dumps
 from cashback.orderHTTP import orderHTTP
 
 # Create your tests here.
 class TestCashbackView(unittest.TestCase):
 
     def test_order_http(self):
-        json = '{ "sold_at": "2026-01-02 00:00:00", "customer": { "document": "16149113710", "name": "JOSE DA SILVA" }, "total": "100.00", "products": [{ "type": "A", "value": "10.00", "qty": 1 }, { "type": "B", "value": "10.00", "qty": 9 }] }'
-        order = orderHTTP(json)
-        self.assertEqual(order.customer.name, 'JOSE DA SILVA')
-        self.assertEqual(order.customer.document, '16149113710')
-        self.assertEqual(order.total, 100)
+        with open('cashback/testInputs/rightInputs.yaml') as json:
+            for obj in load(json):
+                order = orderHTTP(dumps(obj['input']))
+                output = obj['output']
+                self.assertEqual(order.customer.name, output['name'])
+                self.assertEqual(order.customer.document, output['document'])
+                self.assertEqual(order.total, output['total'])
