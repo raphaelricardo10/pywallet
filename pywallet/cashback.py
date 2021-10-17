dfd71f182ef89c6643c829e92d3a880b09067697
Raphael <raphaelricardo10@gmail.com>
2021-10-16 21:11:36 -0300

    Added the cashback class and passed in test

--- pywallet/cashback.py
+++ pywallet/cashback.py
@@ -1,8 +1,37 @@
-def calculateCashback(value: float, percent: float) -> float:
-    if value < 0:
-        raise ValueError("The value cannot be less than zero")
+class Cashback:
+    def __init__(self, total: float, percent: float) -> None:
+        self._percent = percent
+        self.total = total
 
-    if percent < 0:
-        raise ValueError("Percent value cannot be less than zero")
+    @property
+    def percent(self):
+        return self._percent
 
-    return value*percent/float(100)
+    @percent.setter
+    def percent(self, value):
+        if value < 0:
+            raise ValueError("The percent cannot be less than zero")
+            
+        self._percent = value
+        self.value = Cashback.calculateCashback(self.total, self.percent)
+
+    @property
+    def total(self):
+        return self._total
+
+    @total.setter
+    def total(self, value):
+        if value < 0:
+            raise ValueError("The total cannot be less than zero")
+        
+        self._total = value
+        self.value = Cashback.calculateCashback(self.total, self.percent)
+
+    def calculateCashback(total: float, percent: float) -> float:
+        if total < 0:
+            raise ValueError("The value cannot be less than zero")
+
+        if percent < 0:
+            raise ValueError("Percent value cannot be less than zero")
+
+        return total*percent/float(100)
