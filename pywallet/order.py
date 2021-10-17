9c10a153701a829ae78e673f3d77ec1a3d41d430
Raphael <raphaelricardo10@gmail.com>
2021-10-16 21:13:57 -0300

    Cashback in a order test passed

--- pywallet/order.py
+++ pywallet/order.py
@@ -1,17 +1,27 @@
 from datetime import datetime
 from pywallet.product import Product
 from pywallet.customer import Customer
+from pywallet.cashback import Cashback
 
 class Order:
-    def __init__(self, customer: Customer, sold_at: str) -> None:
+    def __init__(self, customer: Customer, sold_at: str, cashbackPercent:float =None) -> None:
         self.customer = customer
         self.sold_at = sold_at
         self.items = []
         self._total = 0
 
+        if cashbackPercent:
+            self.cashbackInstance = Cashback(self.total, cashbackPercent)
+        else:
+            self.cashbackInstance = None
+
     @property
     def total(self):
         return self._total
+
+    @property
+    def cashback(self):
+        return self.cashbackInstance.value
 
     @property
     def sold_at(self):
@@ -33,3 +43,6 @@ class Order:
         newItem = Order.Item(product, qty)
         self.items.append(newItem)
         self._total += newItem.product.value * newItem.qty
+
+        if self.cashbackInstance:
+            self.cashbackInstance.total = self._total
