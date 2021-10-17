d578e13c4f0943a39089aafb63ff21881ca6e6f1
Raphael <raphaelricardo10@gmail.com>
2021-10-17 14:24:22 -0300

    Added test to database class

---
+++ tests/database/testDatabase.py
@@ -0,0 +1,17 @@
+import unittest
+from database import Database
+
+class TestDatabase(unittest.TestCase):
+
+    def test_connection(self):
+        database = Database('pywallet')
+        self.assertNotEqual(database.connection, None)
+
+    def test_connection_error(self):
+        #It is needed to implement a network-blocking method before
+        with self.assertRaises(ConnectionError):
+            Database('pywallet')
+
+    def test_wrong_db_name(self):
+        with self.assertRaises(ValueError):
+            Database('wrong-db-name')
