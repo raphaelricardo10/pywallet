eae5bb6386ad2284cc0f097ce08e63860e808253
Raphael <raphaelricardo10@gmail.com>
2021-10-16 21:09:08 -0300

    Added test to encapsulate cashback in a class

--- tests/testCashback.py
+++ tests/testCashback.py
@@ -1,16 +1,16 @@
 import unittest
-from pywallet.cashback import calculateCashback
+from pywallet.cashback import Cashback
 
 class TestCashbackMethods(unittest.TestCase):
     
     def test_cashback_ok(self):
-        result = calculateCashback(150, 5)
-        self.assertEqual(result, 7.5)
+        cashback = Cashback(150, 5)
+        self.assertEqual(cashback.value, 7.5)
 
     def test_negative_percent(self):
         with self.assertRaises(ValueError):
-            calculateCashback(150, -5)
+            Cashback(150, -5)
 
     def test_negative_value(self):
         with self.assertRaises(ValueError):
-            calculateCashback(-150, 5)
+            Cashback(-150, 5)
