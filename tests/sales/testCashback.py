6e806e8d8cdc7262a2d86cfb662dc2a7d7407a02
Raphael <raphaelricardo10@gmail.com>
2021-10-18 13:36:28 -0300

    Inverted cashback arguments

--- tests/sales/testCashback.py
+++ tests/sales/testCashback.py
@@ -4,16 +4,16 @@ from cashback import cashbackTypes
 class TestCashback(unittest.TestCase):
     
     def test_cashback_ok(self):
-        cashback = cashbackTypes.ConstantByTotal(150, 5)
+        cashback = cashbackTypes.ConstantByTotal(5, 150)
         self.assertEqual(cashback.value, 7.5)
 
     def test_negative_percent(self):
         with self.assertRaises(ValueError):
-            cashbackTypes.ConstantByTotal(150, -5)
+            cashbackTypes.ConstantByTotal(-5, 150)
 
     def test_negative_value(self):
         with self.assertRaises(ValueError):
-            cashbackTypes.ConstantByTotal(-150, 5)
+            cashbackTypes.ConstantByTotal(5, -150)
 
     def test_variable_percents(self):
         breakpoints = [
